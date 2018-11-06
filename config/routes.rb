ab989c8c2d6a6d5af042feb4a10d3491499b301e
Sylvain Galtier <sylvaingaltier@gmail.com>
2018-11-06 17:13:51 +0100

    finished exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :tasks
+  post 'tasks' => 'tasks#create'
+  get 'tasks/new' => 'tasks#new', as: :new_task
+  get '/tasks/:id' => 'tasks#show', as: :task
+  get 'tasks/:id/edit' => 'tasks#edit', as: :edit_task
+  patch 'tasks/:id' => 'tasks#update'
+  delete 'tasks/:id' => 'tasks#destroy'
 end
